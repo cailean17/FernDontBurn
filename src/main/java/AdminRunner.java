663469263a3402600ac963fd8d7b779f60814ed6
Cailean Fernandes <551096@my.cuhsd.org>
2023-03-31 13:04:27 -0700

    Scraping off arudino data file complete

--- src/main/java/AdminRunner.java
+++ src/main/java/AdminRunner.java
@@ -2,7 +2,9 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.util.Scanner;
 
 public class AdminRunner {
 
@@ -11,7 +13,7 @@ public class AdminRunner {
 
         try {
             FileInputStream serviceAccount =
-                    new FileInputStream("D:/Cailean/AP Comp Sci/FernDontBurn/secret.json");
+                    new FileInputStream("src/main/java/secret.json");
 
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
@@ -20,7 +22,20 @@ public class AdminRunner {
             FirebaseApp.initializeApp(options);
 
             APICalls caller = new APICalls();
-            caller.updatePole(new Pole(Math.random(), 20.0, 200.0, "Kooser Utiliy Pole 1"));
+            File arduinoData = new File("src/main/java/ArduinoData.txt");
+            Scanner sc = new Scanner(arduinoData);
+            sc.nextLine();
+            while(sc.hasNextLine()){
+                Scanner lr  = new Scanner(sc.nextLine());
+                lr.useDelimiter(",");
+                String pole_name = lr.next();
+                double Co2_concentration = Double.parseDouble(lr.next());
+                double battery_level = Double.parseDouble(lr.next());
+                double tilt = Double.parseDouble(lr.next());
+                caller.updatePole(new Pole(Co2_concentration, battery_level, tilt, pole_name));
+
+            }
+
         }catch(Exception E){
             E.printStackTrace();
         }
